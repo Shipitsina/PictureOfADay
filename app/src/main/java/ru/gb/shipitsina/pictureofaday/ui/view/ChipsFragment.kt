6f7e880ec89e971273891cca9edb3de02e9dfb0e
Shipitsina <kybeg@inbox.ru>
2021-12-20 09:13:00 +0500

    Урок 1 с изменениями

--- app/src/main/java/ru/gb/shipitsina/pictureofaday/ui/view/ChipsFragment.kt
+++ app/src/main/java/ru/gb/shipitsina/pictureofaday/ui/view/ChipsFragment.kt
@@ -9,14 +9,30 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
 import kotlinx.android.synthetic.main.fragment_chips.*
 import ru.gb.shipitsina.pictureofaday.R
+import ru.gb.shipitsina.pictureofaday.databinding.BottomNavigationLayoutBinding
+import ru.gb.shipitsina.pictureofaday.databinding.FragmentChipsBinding
+import ru.gb.shipitsina.pictureofaday.databinding.MainFragmentBinding
+import ru.gb.shipitsina.pictureofaday.ui.view.picture.PictureOfTheDayFragment
 
 class ChipsFragment : Fragment() {
+
+    private var _binding: FragmentChipsBinding? = null
+    val binding: FragmentChipsBinding
+        get(){
+            return _binding!!
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_chips, container, false)
+    ): View {
+        _binding = FragmentChipsBinding.inflate(inflater, container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,5 +50,10 @@ class ChipsFragment : Fragment() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = ChipsFragment
     }
 }
--- app/src/main/java/ru/gb/shipitsina/pictureofaday/ui/viewmodel/PictureOfTheDayViewModel.kt
+++ app/src/main/java/ru/gb/shipitsina/pictureofaday/ui/viewmodel/PictureOfTheDayViewModel.kt
@@ -6,51 +6,62 @@ import androidx.lifecycle.ViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import ru.gb.shipitsina.pictureofaday.BuildConfig
 import ru.gb.shipitsina.pictureofaday.ui.model.PODRetrofitImpl
 import ru.gb.shipitsina.pictureofaday.ui.model.PODServerResponseData
 
 class PictureOfTheDayViewModel(
-    private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(),
+    private val liveDataForViewToObserve: MutableLiveData<PictureOfTheDayData> = MutableLiveData(), // PictureOfTheDayData - состояние загрузки картинки: успех/загружается/ошибка
     private val retrofitImpl: PODRetrofitImpl = PODRetrofitImpl()
 ) :
     ViewModel() {
 
+    /**
+     * Метод возвращает Данные в PictureOfTheDateFragment, когда с ними что-то происходит (через Observer)
+     */
     fun getData(): LiveData<PictureOfTheDayData> {
         sendServerRequest()
         return liveDataForViewToObserve
     }
 
-    private fun sendServerRequest() {
+    /**
+     * Метод возвращает ответ сервера NASA
+     */
+    fun sendServerRequest() {
         liveDataForViewToObserve.value = PictureOfTheDayData.Loading(null)
         val apiKey: String = BuildConfig.NASA_API_KEY
         if (apiKey.isBlank()) {
             PictureOfTheDayData.Error(Throwable("You need API key"))
         } else {
-            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(object :
-                Callback<PODServerResponseData> {
-                override fun onResponse(
-                    call: Call<PODServerResponseData>,
-                    response: Response<PODServerResponseData>
-                ) {
-                    if (response.isSuccessful && response.body() != null) {
-                        liveDataForViewToObserve.value =
-                            PictureOfTheDayData.Success(response.body()!!)
-                    } else {
-                        val message = response.message()
-                        if (message.isNullOrEmpty()) {
-                            liveDataForViewToObserve.value =
-                                PictureOfTheDayData.Error(Throwable("Unidentified error"))
-                        } else {
-                            liveDataForViewToObserve.value =
-                                PictureOfTheDayData.Error(Throwable(message))
-                        }
-                    }
-                }
+            //enqueue - добавление в очередь. Ответ передаем в callback
+            retrofitImpl.getRetrofitImpl().getPictureOfTheDay(apiKey).enqueue(callback)
+        }
+    }
 
-                override fun onFailure(call: Call<PODServerResponseData>, t: Throwable) {
-                    liveDataForViewToObserve.value = PictureOfTheDayData.Error(t)
+    val callback = object :Callback<PODServerResponseData>{
+        //Если сервер ответил
+        override fun onResponse(
+            call: Call<PODServerResponseData>,
+            response: Response<PODServerResponseData>
+        ) {
+            if (response.isSuccessful && response.body() != null) {
+                //Устанавливаем состояние liveDataForViewToObserve в Success
+                liveDataForViewToObserve.value =
+                    PictureOfTheDayData.Success(response.body()!!)
+            } else {
+                val message = response.message()
+                if (message.isNullOrEmpty()) {
+                    liveDataForViewToObserve.value =
+                        PictureOfTheDayData.Error(Throwable("Unidentified error"))
+                } else {
+                    liveDataForViewToObserve.value =
+                        PictureOfTheDayData.Error(Throwable(message))
                 }
-            })
+            }
+        }
+        //Если сервер не ответил :(
+        override fun onFailure(call: Call<PODServerResponseData>, t: Throwable) {
+            liveDataForViewToObserve.value = PictureOfTheDayData.Error(t)
         }
     }
 }
